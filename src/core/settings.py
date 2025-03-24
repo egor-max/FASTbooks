7178b005d36170890cda0d0ff48753c89183195c
Alost_dos <egorushka.ermolaew@yandex.ru>
2025-03-24 21:53:03 +0300

    settings annotation

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,13 +1,51 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import (
+    BaseSettings, 
+    SettingsConfigDict,
+)
 
 
-class DbSettings(BaseSettings):
-    pass
+class PostgresSqlSettings(BaseSettings):
+    """
+    Настройки для подключения к базе данных.
+    
+    Параметры задаются через переменные окружения, указанные в .env.
 
+    Attributes 
+    ----------
+    DATABASE_URL : str
+        URL подключение к базе данных.
+    
+    """
+
+    database_url: str
+    model_config = SettingsConfigDict(      #  Изучить SettingsConfigDict
+                                            #  Установить зависимость 
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        
+    )
+    
 
 class AppSettings(BaseSettings):
-    pass
+    """
+    Настройки параметров FastAPI приложения.
+
+    Параметры задаются через переменные окружения, указанные в .env.
+
+    Attributes 
+    ----------
+
+    """
 
 
-class settings(BaseSettings, DbSettings, AppSettings):
-    pass
+class Settings(BaseSettings, PostgresSqlSettings, AppSettings): 
+    """
+    Настройки проекта для запуска приложения.
+
+    Параметры задаются через родительские классы.
+
+    Attributes 
+    ----------
+    None
+
+    """
